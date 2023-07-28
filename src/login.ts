7bce0a5498d26b26cb890d704197f3053912a298
Rhydian Davies <rhydian@Rhydians-MacBook-Air.local>
2023-07-28 15:48:08 +0100

    chore: configure login lambda

--- src/login.ts
+++ src/login.ts
@@ -1,11 +1,26 @@
+import { missingEnvVarsErrorMessage } from "./constants";
+
 exports.handler = async () => {
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      message: "Hello world!",
-    }),
-  };
+  try {
+    const { CLIENT_ID, REDIRECT_URI, SCOPE } = process.env;
+
+    if (!CLIENT_ID || !REDIRECT_URI || !SCOPE) {
+      throw new Error(missingEnvVarsErrorMessage);
+    }
+
+    return {
+      statusCode: 302,
+      headers: {
+        Location: `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}`,
+      },
+    };
+  } catch (err) {
+    if (err) {
+      console.log(err);
+      return {
+        statusCode: 500,
+        body: err instanceof Error ? err.message : err,
+      };
+    }
+  }
 };
